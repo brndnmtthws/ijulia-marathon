9b4192f3f174795d26e6184acd8555798e7d72c2
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-18 23:15:46 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -5,4 +5,7 @@
 #the docker image to run more that one software for expecified configuration like when you need mysql running to chnage or create
 #database for the container ...
 
-#julia -e 'Pkg.add("IJulia")'
+julia -e 'Pkg.add("IJulia")'
+
+#to make sure don't return with error from installing IJulia
+echo 'done pre-configuration'
