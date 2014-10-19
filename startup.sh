9211a877978aaaa8852bb6933b8d4ff8f5a341c3
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-18 22:20:06 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,11 +4,11 @@
 set -e
 
 if [ -f /etc/configured ]; then
-        ipython notebook --profile julia &
+        #ipython notebook --profile julia &
         echo 'already configured'
 else
       #code that need to run only one time ....
-        ipython notebook --profile julia &
+        #ipython notebook --profile julia &
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
