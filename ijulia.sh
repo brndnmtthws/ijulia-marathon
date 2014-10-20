6743d961090d73a0093eb0b6bf9fd6bafbfb6299
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-20 13:17:30 -0400

    Update ijulia.sh

--- ijulia.sh
+++ ijulia.sh
@@ -3,9 +3,5 @@
 # `/sbin/setuser xxxxx` runs the given command as the user `xxxxxx`.
 # If you omit that part, the command will be run as root.
 
-stdout_logfile_backups = 2
-stdout_logfile_maxbytes = 1MB
-stderr_logfile_backups = 2
-stderr_logfile_maxbytes = 1MB
 
 exec ipython notebook --profile julia >>/var/log/ijulia.log 2>&1
