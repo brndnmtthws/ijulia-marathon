dc3fb2d3834f6c62fbc6c076c87547441c800968
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-20 19:01:05 -0400

    Delete tornado.sh

--- tornado.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-### In tornado.sh (make sure this file is chmod +x):
-# `/sbin/setuser xxxxx` runs the given command as the user `xxxxxx`.
-# If you omit that part, the command will be run as root.
-
-directory=/root/.juliabox/tornado
-
-exec /root/.juliabox/tornado/src/fmanage.py >>/var/log/tornado.log 2>&1
