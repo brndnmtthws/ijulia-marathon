277dc9fd4a6fb223ff5a0841308f065afe72aeeb
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-20 10:47:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,9 @@ if [ -f /etc/configured ]; then
 else
       #code that need to run only one time ....
         #needed for fix problem with ubuntu and cron
+        echo "c.NotebookApp.open_browser = False" >> /root/.ipython/profile_julia/ipython_notebook_config.py
+        echo "c.NotebookApp.ip = \"*\"" >> /root/.ipython/profile_julia/ipython_notebook_config.py
+        echo "c.NotebookApp.allow_origin = \"*\"" >> /root/.ipython/profile_julia/ipython_notebook_config.py
         update-locale 
         date > /etc/configured
 fi
