d2c3f4bf32815ff0d102bdb9c2e053cd803624ac
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-20 10:41:01 -0400

    Update ijulia.sh

--- ijulia.sh
+++ ijulia.sh
@@ -8,4 +8,4 @@ stdout_logfile_maxbytes = 1MB
 stderr_logfile_backups = 2
 stderr_logfile_maxbytes = 1MB
 
-exec ipython notebook --profile julia >>/var/log/ijulia.log 2>&1
+exec ipython notebook -- --profile julia >>/var/log/ijulia.log 2>&1
