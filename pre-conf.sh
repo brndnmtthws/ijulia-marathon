d0df2c9bc15f2852985325597b5836646454af1a
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-21 14:44:05 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -8,7 +8,6 @@
 python -m IPython.external.mathjax
 
 ipython profile create julia
-ipython profile create nbserver
 
 julia -e 'Pkg.init(); Pkg.add("IJulia"); Pkg.add("PyPlot"); Pkg.add("SIUnits"); Pkg.add("Gadfly"); Pkg.add("DataStructures"); Pkg.add("HDF5"); Pkg.add("Iterators"); Pkg.add("MCMC"); Pkg.add("NumericExtensions"); Pkg.add("SymPy"); Pkg.add("Interact"); Pkg.add("Roots");'
 
