1ff728a4c565e621dd3c5131f8734daf7e6a11ac
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-22 13:51:24 -0400

    Update ijulia.sh

--- ijulia.sh
+++ ijulia.sh
@@ -6,4 +6,13 @@
 export HOME=/root
 cd /root
 
-exec julia -e "using IJulia;notebook();" >>/var/log/ijulia.log 2>&1
+
+
+if [ -f /etc/secured ]; then
+        echo 'already secured'
+        exec julia -e "using IJulia;notebook();" >>/var/log/ijulia.log 2>&1
+else
+        echo 'need to secured by running docker exec -it container_id after_install to be able to used it'
+        echo 'need to secured by running docker exec -it container_id after_install to be able to used it' >> /var/log/ijulia.log 2>&1
+fi
+
