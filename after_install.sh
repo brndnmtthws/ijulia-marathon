e8049501a26219028d675c4464efa8901f855df8
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-22 00:48:24 -0400

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -2,11 +2,10 @@
 
 
 PASSWORD = $(python -c "from IPython.lib import passwd;a=passwd(); print a")
-echo 'c.NotebookApp.password = u$PASSWORD' >> /root/.ipython/profile_julia/ipython_notebook_config.py
+echo "c.NotebookApp.password = u$PASSWORD" >> /root/.ipython/profile_julia/ipython_notebook_config.py
 
 mkdir -p /root/.ssl
 openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /root/.ssl/mycert.pem -out /root/.ssl/mycert.pem
 
 echo "c.NotebookApp.certfile = u'/root/.ssl/mycert.pem'" >> /root/.ipython/profile_julia/ipython_notebook_config.py
-
 
