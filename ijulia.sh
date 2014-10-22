63355b7d894dd4a360cca9731716dd4f4d930bfc
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-22 00:38:11 -0400

    Update ijulia.sh

--- ijulia.sh
+++ ijulia.sh
@@ -3,5 +3,6 @@
 # `/sbin/setuser xxxxx` runs the given command as the user `xxxxxx`.
 # If you omit that part, the command will be run as root.
 
+printenv
 
 exec /bin/bash -c "cd /root ; julia -e 'using IJulia;notebook();'" >>/var/log/ijulia.log 2>&1
