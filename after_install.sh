fb7c946de12a5a6781356fc7f9a6f0a34f7bf9e9
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-22 13:46:00 -0400

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -9,4 +9,5 @@ openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout /root/.ssl/mycert.pe
 
 echo "c.NotebookApp.certfile = u'/root/.ssl/mycert.pem'" >> /root/.ipython/profile_julia/ipython_notebook_config.py
 
+date > /etc/secured
 rm -f /sbin/after_install
