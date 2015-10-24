5b4582cabb92aed114d791a75a87f1f3c8eaeeb0
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-10-24 14:23:42 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,7 +10,8 @@ else
         #needed for fix problem with ubuntu and cron
         echo "c.IPKernelApp.pylab = 'inline'" >> /root/.ipython/profile_julia/ipython_notebook_config.py
         echo "c.NotebookApp.open_browser = False" >> /root/.ipython/profile_julia/ipython_notebook_config.py
-        echo "c.NotebookApp.ip = \"*\"" >> /root/.ipython/profile_julia/ipython_notebook_config.py
+        echo "c.NotebookApp.ip = \"0.0.0.0\"" >> /root/.ipython/profile_julia/ipython_notebook_config.py
+        echo "c.NotebookApp.port = 8998" >> /root/.ipython/profile_julia/ipython_notebook_config.py
         echo "c.NotebookApp.allow_origin = \"*\"" >> /root/.ipython/profile_julia/ipython_notebook_config.py
         update-locale 
         date > /etc/configured
